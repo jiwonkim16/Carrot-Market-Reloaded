0d45127d05795b03cf9dbe6987770bf798237cf1
Kim Jiwon <stayby16@kakao.com>
2024-04-08 18:49:27 +0900

    Server Actions

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,15 +3,27 @@ import FormButton from "../../components/form-btn";
 import SocialLogin from "@/components/social-login";
 
 function Login() {
+  const onSubmit = async (formData: FormData) => {
+    "use server";
+    console.log(formData.get("email"), formData.get("password"));
+    console.log("i am server baby");
+  };
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput required type="email" placeholder="Email" errors={[]} />
+      <form action={onSubmit} className="flex flex-col gap-3">
         <FormInput
+          name="email"
+          required
+          type="email"
+          placeholder="Email"
+          errors={[]}
+        />
+        <FormInput
+          name="password"
           required
           type="password"
           placeholder="Password"
--- components/form-input.tsx
+++ components/form-input.tsx
@@ -3,12 +3,20 @@ interface FormInputProps {
   placeholder: string;
   required: boolean;
   errors: string[];
+  name: string;
 }
 
-function FormInput({ type, placeholder, required, errors }: FormInputProps) {
+function FormInput({
+  type,
+  placeholder,
+  required,
+  errors,
+  name,
+}: FormInputProps) {
   return (
     <div className="flex flex-col gap-2">
       <input
+        name={name}
         type={type}
         placeholder={placeholder}
         required={required}
