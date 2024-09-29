09e1300e6b597717d56aa75a3b51e14023030872
Kim Jiwon <stayby16@kakao.com>
2024-09-29 15:58:57 +0900

    🔥 Prisma relation

--- lib/db.ts
+++ lib/db.ts
@@ -3,14 +3,16 @@ import { PrismaClient } from "@prisma/client";
 const db = new PrismaClient();
 
 async function test() {
-  const user = await db.user.create({
-    data: {
-      username: "test",
+  const token = await db.sMSToken.findUnique({
+    where: {
+      id: 1,
+    },
+    include: {
+      user: true,
     },
   });
-  console.log(user);
+  console.log(token);
 }
-
 test();
 
 export default db;
